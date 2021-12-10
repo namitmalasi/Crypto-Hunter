602e7d27227a89b1db2fe030f74cc9e43a49cc17
Namit Malasi <namitmalasi@gmail.com>
2021-12-10 15:55:52 +0530

    added nabber in home page

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,60 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: "25",
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "grey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get All The Info Regarding Your Favorite Crypto Currency
+          </Typography>
+        </div>
+
+        <Carousel />
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
